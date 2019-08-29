72fffd1c0adb7929b1e4b41724fc48a7d30b400f
duhyundev <48434093+duhyundev@users.noreply.github.com>
2019-08-29 20:18:33 +0900

    useEffect

--- react-hooks-tutorial/src/App.js
+++ react-hooks-tutorial/src/App.js
@@ -6,7 +6,7 @@
 
    ? introduction hooks : https://youtu.be/dpw9EHDh2bM
 */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   // * useState는 hooks이다. useState는 유사 [this.state, setState] 꼴 과 같은 pair를 return한다.
@@ -18,11 +18,19 @@ function App() {
    *    setCount는 count 변수만을 위한 setState()와 비슷하게 이해될 수 있다.
    * ? setState(argument)
    *    setState의 argument는 local state의 값을 초기화할 수 있다. 이러한 초기 argument는 첫번째 render시에만 사용된다.
-   *    또한, 하나의 component 안에서 useState는 여러번 사용될 수 있다.
+   *    또한, 하나의 component 안에서 여러개의 state 변수를 선언할 수 있다.
    */
   const [age, setAge] = useState(28);
   const [data, setDate] = useState({ year: 2019, month: 'August', day: 29 });
   // ...
+
+  /**
+   * ? useEffect
+   *    useEffect는 componentsDidMount, componentsDidUpdate와 유사하다.
+   */
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
 
   return (
     <div>
