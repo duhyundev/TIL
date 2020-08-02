22d2d9900e3b12fb056c1dae02821f08bbe34d11
duhyunkim` <duhyun.dev@gmail.com>
2020-08-02 18:31:35 +0900

    Feat : new client landed🚀
    - init create-react-app init with typescript
    - init storybook with atomic patterns
    - add react-markdown contentView Component
      - add codeBlockRenderer
      - add custom styled wrapper

---
+++ client/src/helpers/CodeBlockRenderer.stories.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import CodeBlockRenderer from './CodeBlockRenderer';
+
+export default {
+  title: 'renderers|CodeBlockRenderer',
+  component: CodeBlockRenderer,
+};
+
+const exampleValue = `import * as React from 'react';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/github.css';
+import styled from 'styled-components';
+
+const CodeBlockRendererWrapper = styled.div\`
+  pre {
+    border: 1px solid #ccc;
+  }
+\`;
+
+interface ICodeBlockRendererProps {
+  value: string;
+  language: string;
+}
+
+class CodeBlockRenderer extends React.PureComponent<ICodeBlockRendererProps> {
+  constructor(props: ICodeBlockRendererProps) {
+    super(props);
+    this.setRef = this.setRef.bind(this);
+  }
+
+  private codeEl: HTMLElement | undefined;
+
+  private setRef(el: HTMLElement) {
+    this.codeEl = el;
+  }
+
+  componentDidMount() {
+    this.highlightCode();
+  }
+
+  componentDidUpdate() {
+    this.highlightCode();
+  }
+
+  highlightCode() {
+    if (this.codeEl) {
+      hljs.highlightBlock(this.codeEl);
+    }
+  }
+
+  render() {
+    return (
+      <CodeBlockRendererWrapper>
+        <pre>
+          <code ref={this.setRef} className={\`language-\${this.props.language}\`}>
+            {this.props.value}
+          </code>
+        </pre>
+      </CodeBlockRendererWrapper>
+    );
+  }
+}
+
+export default CodeBlockRenderer;
+
+`;
+
+export const ExampleRenderer = () => (
+  <CodeBlockRenderer language="js" value={exampleValue} />
+);
