5d088b8d4882b64daeca5494591ca43d284198cd
duhyunkim <duhyun.dev@gmail.com>
2020-10-09 18:04:42 +0900

    WIP

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,59 +1,14 @@
 import React from 'react';
-import ContentView from './atoms/ContentView';
-
-const mock = `
-# Live demo
-
-Changes are automatically rendered as you type.
-
-## Table of Contents
-
-* Implements [GitHub Flavored Markdown](https://github.github.com/gfm/)
-* Renders actual, "native" React DOM elements
-* Allows you to escape or skip HTML (try toggling the checkboxes above)
-* If you escape or skip the HTML, no \`dangerouslySetInnerHTML\` is used! Yay!
-
-## HTML block below
-
-<blockquote>
-  This blockquote will change based on the HTML settings above.
-</blockquote>
-
-## How about some code?
-\`\`\`js
-var React = require('react');
-var Markdown = require('react-markdown');
-
-React.render(
-  <Markdown source="# Your markdown here" />,
-  document.getElementById('content')
-);
-\`\`\`
-
-Pretty neat, eh?
-
-## Tables?
-
-| Feature   | Support |
-| --------- | ------- |
-| tables    | ✔ |
-| alignment | ✔ |
-| wewt      | ✔ |
-
-## More info?
-
-Read usage information and more on [GitHub](//github.com/rexxars/react-markdown)
-
----------------
-
-A component by [Espen Hovlandsdal](https://espen.codes/)
-`;
 
 function App() {
   return (
-    <div>
-      <div>HelloWorld</div>
-      <ContentView source={mock} />
+    <div style={{ margin: '100px' }}>
+      <h2>Hello duhyunkim World</h2>
+      <div style={{ margin: '50px' }}>
+        <h3>🙋🏻‍♂️ duhyunkim</h3>
+        <h3>☎️ 010-5160-5309</h3>
+        <h3>📮 duhyun.dev@gmail.com</h3>
+      </div>
     </div>
   );
 }
